b8764ee242cb84f6bd9af0936b5f43c68e2fbd98
ArduFish123 <80416599+ArduFish123@users.noreply.github.com>
2024-07-13 11:31:39 +0000

    logger

--- src/main/java/eu/raspberrymods/fishlib/FishLib.java
+++ src/main/java/eu/raspberrymods/fishlib/FishLib.java
@@ -17,6 +17,6 @@ public class FishLib implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.warn("This is a pretty fishy library mod!");
 	}
 }
