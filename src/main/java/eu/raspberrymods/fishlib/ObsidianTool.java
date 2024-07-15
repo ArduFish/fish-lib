ce56d76aa01d543017efc9f2b80d7e3e4bdd5b06
ArduFish123 <80416599+ArduFish123@users.noreply.github.com>
2024-07-15 14:16:45 +0000

    bruh

--- src/main/java/eu/raspberrymods/fishlib/ObsidianTool.java
+++ src/main/java/eu/raspberrymods/fishlib/ObsidianTool.java
@@ -1,4 +1,4 @@
-import net.minecraft.*;
+//import net.minecraft.*;
 @Override
 public class ObsidianTool implements ToolMaterial {
 	// Your IDE should override the interface's methods for you, or at least shout at you to do so.
