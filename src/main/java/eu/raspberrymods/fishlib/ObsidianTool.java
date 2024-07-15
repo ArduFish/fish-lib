de1e425108b3471aa6c3988860adb652fd8e9ae8
ArduFish123 <80416599+ArduFish123@users.noreply.github.com>
2024-07-15 14:12:34 +0000

    2

--- src/main/java/eu/raspberrymods/fishlib/ObsidianTool.java
+++ src/main/java/eu/raspberrymods/fishlib/ObsidianTool.java
@@ -1,4 +1,28 @@
 @Override
 public class ObsidianTool implements ToolMaterial {
 	// Your IDE should override the interface's methods for you, or at least shout at you to do so.
+    @Override
+    public int getDurability() {
+	    return 1500;
+    }
+    @Override
+    public float getMiningSpeedMultiplier() {
+	    return 1.0F;
+    }
+    @Override
+    public float getAttackDamage() {
+	    return 15.0F;
+    }
+    @Override
+    public TagKey<Block> getInverseTag() {
+	    return BlockTags.INCORRECT_FOR_STONE_TOOL;
+    }
+    @Override
+    public int getEnchantability() {
+	    return 22;
+    }
+    @Override
+    public Ingredient getRepairIngredient() {
+    	return Ingredient.ofItems(Items.OBSIDIAN);
+    }
 }
